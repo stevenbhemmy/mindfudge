a8a1df9652a182e785d99d35d6b5541dce040825
Steven Hemmy <stevenbhemmy@gmail.com>
2026-04-20 22:39:36 -0500

    add Interpreter (stubbed impl) and Lexer (finished)

---
+++ Interpreter/Interpreter.h
@@ -0,0 +1,47 @@
+#ifndef __INTERPRETER_H
+#define __INTERPRETER_H
+
+#include <istream>
+#include <functional>
+#include <map>
+#include <vector>
+
+#include <fmt/core.h>
+
+#include <Lexer.h>
+
+
+class Interpreter {
+    public:
+        Interpreter(std::vector<Token *> instrs) {
+            fmt::println("Setting up interpreter");
+            instructions = instrs;
+            pointer = 0;
+        }
+            
+        void interpret() {
+            fmt::println("Running interpreter");
+            for (auto instr : instructions) {
+                dispatchTable[instr->TYPE]();
+            }
+        }
+
+    private:
+        unsigned char tape[30000] = { 0 };
+        int pointer;
+
+        std::vector<Token *> instructions;
+
+        std::map<enum type, std::function<void()>> dispatchTable{
+            {TOKEN_PLUS, [this](){ tape[pointer] += 1; fmt::println("Incr tape[pointer]"); } },
+            {TOKEN_MINUS, [this](){ tape[pointer] -= 1; fmt::println("Decr tape[pointer]"); } },
+            {TOKEN_LT, [this](){ pointer -= 1; fmt::println("Decr pointer"); } },
+            {TOKEN_GT, [this](){ pointer += 1; fmt::println("Incr pointer"); } },
+            {TOKEN_LB, [this](){ fmt::println("Start loop"); /* TODO */ } },
+            {TOKEN_RB, [this](){ fmt::println("End loop"); /* TODO */ } },
+            {TOKEN_DOT, [this](){ /* fmt::print(tape[pointer]); */ fmt::println("Print tape[pointer]"); } },
+            {TOKEN_COMMA, [this](){ /* cin.get(tape[pointer]); */ fmt::println("Get tape[pointer]"); } },
+        };
+};
+
+#endif
--- mindfudge.cpp
+++ mindfudge.cpp
@@ -1,6 +1,42 @@
+#include <fstream>
+#include <sstream>
+#include <vector>
+
 #include <fmt/core.h>
 
-int main() {
-	fmt::print("Hello World!\n");
+#include <Lexer.h>
+#include <Interpreter.h>
+
+int main(int argc, char * argv[]) {
+
+    if (argc < 2) {
+        fmt::println("No source file specified, exiting.");
+        exit(1);
+    }
+
+    fmt::println("argv[1] = {}", argv[1]);
+
+    std::ifstream sourceFile (argv[1]);
+    std::stringstream buffer;
+
+    char temp;
+    while(sourceFile.get(temp)) {
+        buffer << temp;
+    }
+
+    fmt::print(buffer.str());
+
+    auto lexer = Lexer(buffer.str());
+
+    std::vector<Token *> tokens;
+    tokens = lexer.tokenize();
+
+    /* for (const auto& token : tokens) { */
+    /*     fmt::println(typeToString(token->TYPE)); */
+    /* } */
+
+    auto interpreter = Interpreter(tokens);
+    interpreter.interpret();
+    
 	return 0;
-}
+};
