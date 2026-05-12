169a3de053d1f15acded9d96850e2c7e7903bf90
Steven Hemmy <stevenbhemmy@gmail.com>
2026-05-11 21:46:54 -0500

    add tokenization test

--- mindfudge_tests.cpp
+++ mindfudge_tests.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include <catch2/catch_test_macros.hpp>
 
 #include <Lexer.h>
@@ -13,6 +15,34 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
   REQUIRE(factorial(10) == 3'628'800);
 }
 
-TEST_CASE("Type to string", "[typeToString]") {
+TEST_CASE("Type to string", "[lexer][typeToString]") {
   REQUIRE(typeToString(TOKEN_PLUS) == "TOKEN_PLUS");
+  REQUIRE(typeToString(TOKEN_MINUS) == "TOKEN_MINUS");
+  REQUIRE(typeToString(TOKEN_LT) == "TOKEN_LT");
+  REQUIRE(typeToString(TOKEN_GT) == "TOKEN_GT");
+  REQUIRE(typeToString(TOKEN_LB) == "TOKEN_LB");
+  REQUIRE(typeToString(TOKEN_RB) == "TOKEN_RB");
+  REQUIRE(typeToString(TOKEN_DOT) == "TOKEN_DOT");
+  REQUIRE(typeToString(TOKEN_COMMA) == "TOKEN_COMMA");
+}
+
+TEST_CASE("Lexer tokenize basic bf program", "[lexer][tokenize]") {
+
+  Lexer lex = Lexer(">>++--<<[].,");
+  std::vector<Token *> tokens = lex.tokenize();
+
+  std::vector<Token *> expected_tokens = {
+      new Token{TOKEN_GT, ">"},    new Token{TOKEN_GT, ">"},
+      new Token{TOKEN_PLUS, "+"},  new Token{TOKEN_PLUS, "+"},
+      new Token{TOKEN_MINUS, "-"}, new Token{TOKEN_MINUS, "-"},
+      new Token{TOKEN_LT, "<"},    new Token{TOKEN_LT, "<"},
+      new Token{TOKEN_LB, "["},    new Token{TOKEN_RB, "]"},
+      new Token{TOKEN_DOT, "."},   new Token{TOKEN_COMMA, ","},
+  };
+
+  REQUIRE(std::equal(tokens.begin(), tokens.end(), expected_tokens.begin(),
+                     expected_tokens.end(),
+                     [](const Token *tok, const Token *ex_tok) {
+                       return tok->TYPE == ex_tok->TYPE;
+                     }));
 }
